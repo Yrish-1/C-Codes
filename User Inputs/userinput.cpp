71031b3176dc1cd9963dce4ae60f2eb190dcf007
Yrish-1 <pineyrish@gmail.com>
2026-01-16 21:12:27 +0800

    user input lesson

---
+++ User Inputs/userinput.cpp
@@ -0,0 +1,16 @@
+//the lesson for today is CIN, it is a predefined variable with ">>" insertion operator
+//it is used to get the user's input
+//word is IN for input
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x; //the x will act as the place holder for the integer that you will insert
+    cout<<"My favorite number: "; //this will be displayed
+    cin >> x; //you will place your int here
+    cout<<"The number is "<< x; 
+
+    return 0;
+}
