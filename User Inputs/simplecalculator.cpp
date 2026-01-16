04716456076e6921b0d3cfc0c9ec261279e20ba7
Yrish-1 <pineyrish@gmail.com>
2026-01-16 21:59:50 +0800

    simple addition calculator(will re-edit)

---
+++ User Inputs/simplecalculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int x, y;
+int sum;
+cout<<"Enter 1st number: ";
+cin >> x ;
+cout << "Enter 2nd number: ";
+cin >> y;
+
+cout <<endl;
+sum = x + y;
+cout <<x<<" + "<<y<<" = " << sum << endl;
+
+return 0;
+
+}
